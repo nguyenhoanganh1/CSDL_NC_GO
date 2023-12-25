a4360f2961b0f530219641fb8e839aa6c1b01298
anh.nguyen <bstar.anh.nguyen@finos.asia>
2023-12-26 00:17:24 +0700

    init connect orient db

--- config/orient_db_config.go
+++ config/orient_db_config.go
@@ -7,11 +7,17 @@ type OrientDBService interface {
 }
 
 type OrientDBConfig struct {
+	host         string              `default:"localhost"`
+	port         string              `default:"2424"`
+	databaseName string              `default:"demo"`
+	documentType orient.DatabaseType `default:"document"`
+	username     string              `default:"root"`
+	password     string              `default:"root"`
 }
 
 func (o *OrientDBConfig) GetConnect() orient.DBSession {
 	var db orient.DBConnection
-	open, err := db.Open("demo", "document", "root", "admin")
+	open, err := db.Open(o.databaseName, o.documentType, o.username, o.password)
 	if err != nil {
 		return nil
 	}
